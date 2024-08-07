8224b6cb2f93d1f27d8c575472c42e60c053d375
Gabriel Alexander <gabriel.alexander@sma-yosuka.sch.id>
2024-08-07 11:40:07 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Program Data Kelompok";
+	cin.ignore();
+	cout << endl;
+	cout << "NIS" << endl;
+	cout << "1. 23241014\n"
+		"2. 00000008\n"
+		"3. 23241042\n"
+		"4. 23241061" << endl;
+	cin.ignore();
+	cout << "Nama" << endl;
+	cout << "1. Aurelia\n"
+		"2. David\n"
+		"3. Gabriel\n"
+		"4. Julyana" << endl;
+	cin.ignore();
+	cout << "Kelas 11.2" << endl;
+	cin.ignore();
+	cout << "Jurusan IPA" << endl;
+	cin.ignore();
+	cout << "Jenis Kelamin" << endl;
+	cout << "1. Aurelia (P)\n"
+		"2. David (L)\n"
+		"3. Gabriel (L)\n"
+		"4. Julyana (P)" << endl;
+	cin.ignore();
+	cout << "Alamat" << endl;
+	cout << "1. Aurelia - Cluster Grand Mediterania H1/5\n"
+		"2. David - Bantar Kaung No. 9\n"
+		"3. Gabriel - Perumahan Bumi Taruno Permai Blok D9/11\n"
+		"4. Julyana - Cluster Victorian H2/2" << endl;
+	system("PAUSE");
+	return 0;
+}
